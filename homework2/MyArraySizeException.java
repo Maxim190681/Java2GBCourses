4e90b8adb792bfab236866936b1e1ed8b58fa23c
Zhegunov_Maxim <zhegunov.m@gmail.com>
2022-02-23 19:49:46 +0300

    HW for Lesson2

---
+++ homework2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package geekbrains.jmaxim1906.Java2.homework2;
+
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException() {
+        super("Massive size is incorrect! ");
+    }
+}
